22612acb85a200b6abefedad23e62499b1c56dff
Valentine <Valentin.newak2@gmail.com>
2018-12-10 19:58:56 +0200

    Промежуточный варинат реализации класса Feature

---
+++ TaskTreking/Feture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTreking
+{
+    class Feture : ITask
+    {
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+    }
+}
